0ad2b1cdfb268ec4a98836bcb10e87b110d29f40
Jeremy <56400436+Jeremy-code254@users.noreply.github.com>
2024-02-13 21:16:31 +0300

    tested the api

--- remote/build.gradle.kts
+++ remote/build.gradle.kts
@@ -67,4 +67,8 @@ dependencies {
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.serialization.kotlinx.json)
+    implementation (libs.ktor.client.logging)
+    implementation(libs.ktor.client.mock.jvm)
+    testImplementation(libs.mock.client)
+
 }
